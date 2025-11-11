e664540ec34e44acc7dcdbc4f9c3f605cdfd6acf
August <76185140+Augustina1995@users.noreply.github.com>
2025-11-11 16:42:10 +0200

    SQL practice

---
+++ duomenuBazes/week1/11_10/ComputerStore_questions.sql
@@ -0,0 +1,17 @@
+-- 0 Select all products
+
+	-- SELECT * FROM computerstore.products;
+	
+-- 1 Select the names of all the products in the store.
+-- 2 Select the names and the prices of all the products in the store.
+-- 3 Select the name of the products with a price less than or equal to $200.
+-- 4 Select all the products with a price between $60 and $120.
+-- 5 Select the name and price in cents (i.e., the price must be multiplied by 100).
+-- 6 Select all the products whose name begins with an "DVD"
+-- 7 Select all the products whose name ends with an "e"
+-- 8 Select all the products whose names second letter is "o"
+-- 9 Compute the average price of all the products.
+-- 10 Compute the average price of all products with manufacturer code equal to 2.
+-- 11 Compute the number of products with a price larger than or equal to $180.
+-- 12 Select the name and price of all products with a price larger than or equal to $180, and sort first by price (in descending order), and then by name (in ascending order).
+
